c2d214c2c3f3b4d9330f4411c6b7a926f5164aa0
iamshanedoyle <shanebryandoyle@gmail.com>
2014-01-05 21:35:23 +0000

    More updates, such as added the scores.

--- TheUltimateFactsGame/FactsGame/src/main/java/ie/iamshanedoyle/factsgame/activities/HomeActivity.java
+++ TheUltimateFactsGame/FactsGame/src/main/java/ie/iamshanedoyle/factsgame/activities/HomeActivity.java
@@ -3,6 +3,7 @@ package ie.iamshanedoyle.factsgame.activities;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.Button;
+import android.widget.Toast;
 
 import ie.iamshanedoyle.factsgame.R;
 
@@ -30,7 +33,7 @@ public class HomeActivity extends Activity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements View.OnClickListener {
 
         public PlaceholderFragment() {
         }
@@ -40,6 +43,39 @@ public class HomeActivity extends Activity {
                 Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_home, container, false);
             return rootView;
+        }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+
+            Button buttonPlayNow = (Button) getView().findViewById(R.id.buttonPlayNow);
+            Button buttonHowToPlay = (Button) getView().findViewById(R.id.buttonHowToPlay);
+            Button buttonScores = (Button) getView().findViewById(R.id.buttonScores);
+            Button buttonSettings = (Button) getView().findViewById(R.id.buttonSettings);
+
+            buttonPlayNow.setOnClickListener(this);
+            buttonHowToPlay.setOnClickListener(this);
+            buttonScores.setOnClickListener(this);
+            buttonSettings.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.buttonPlayNow:
+                    startActivity(new Intent(getActivity(), GameActivity.class));
+                    break;
+                case R.id.buttonHowToPlay:
+                    startActivity(new Intent(getActivity(), HowToPlayActivity.class));
+                    break;
+                case R.id.buttonScores:
+                    startActivity(new Intent(getActivity(), ScoresActivity.class));
+                    break;
+                case R.id.buttonSettings:
+                    Toast.makeText(getActivity(), "Soon to be added", Toast.LENGTH_SHORT).show();
+                    break;
+            }
         }
     }
 }
